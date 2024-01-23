441b2814cfacd4f606cd7c66c8ad6c9f887b71e2
Tim Noorlander <tgnoorlander@gmail.com>
2024-01-23 13:12:58 +0100

    hook up tab bar to react-router

--- src/components/layout/TabBar.tsx
+++ src/components/layout/TabBar.tsx
@@ -6,19 +6,36 @@ import {
 import ForestIcon from "@mui/icons-material/Forest";
 import AddIcon from "@mui/icons-material/Add";
 import PersonIcon from "@mui/icons-material/Person";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export function TabBar() {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  function onNavigate(
+    _event: React.SyntheticEvent<Element, Event>,
+    newPath: string
+  ) {
+    navigate(newPath);
+  }
+
   return (
-    <StyledBottomNavigation
-      showLabels
-      value="Recents"
-      onChange={(event, newValue) => {
-        // setValue(newValue);
-      }}
-    >
-      <BottomNavigationAction label="Activities" icon={<ForestIcon />} />
-      <BottomNavigationAction label="Create activity" icon={<AddIcon />} />
-      <BottomNavigationAction label="Profile" icon={<PersonIcon />} />
+    <StyledBottomNavigation showLabels value={pathname} onChange={onNavigate}>
+      <BottomNavigationAction
+        label="Activities"
+        value="/"
+        icon={<ForestIcon />}
+      />
+      <BottomNavigationAction
+        label="Create activity"
+        value="/create-new-activity"
+        icon={<AddIcon />}
+      />
+      <BottomNavigationAction
+        label="Profile"
+        value="/profile"
+        icon={<PersonIcon />}
+      />
     </StyledBottomNavigation>
   );
 }
